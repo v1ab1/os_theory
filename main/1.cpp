f59393c3c060f9627f257c2274cf537c428c5373
v1ab1 <sharkandmouse@gmail.com>
2024-01-21 02:03:06 +0300

    fixed first

--- main/1.cpp
+++ main/1.cpp
@@ -3,41 +3,30 @@
 #include <vector>
 #include <mutex>
 
-// Функция потока, принимающая ссылку на аргумент и изменяющая его
 void threadFunction(int &arg, std::mutex &mu) {
     std::lock_guard<std::mutex> guard(mu);
-    // Изменяем аргумент
     arg += 5;
-    // Распечатываем информацию о потоке и измененном аргументе
     std::cout << "ID потока: " << std::this_thread::get_id() << ", измененное значение: " << arg << std::endl;
 }
 
 int main() {
-    // Количество потоков
     const int numThreads = 5;
-    // Исходный аргумент
     int argument = 0;
 
-    // Мьютекс для синхронизации доступа к аргументу
     std::mutex mu;
 
-    // Вектор для хранения потоков
     std::vector<std::thread> threads;
 
-    // Запускаем несколько потоков
     for (int i = 0; i < numThreads; ++i) {
-        // Создаем и запускаем поток, передаем аргумент по ссылке
         threads.emplace_back(threadFunction, std::ref(argument), std::ref(mu));
     }
 
-    // Дожидаемся завершения всех потоков
     for (std::thread &th : threads) {
         if (th.joinable()) {
             th.join();
         }
     }
 
-    // Печатаем конечное значение аргумента после работы всех потоков
     std::cout << "Финальное значение после остановки всех потоков: " << argument << std::endl;
 
     return 0;
